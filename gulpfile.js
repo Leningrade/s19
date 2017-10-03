7b0fe0683d80d243eed3a299577f11ab82ef2010
Leningrade <edgar.alfa@hotmail.com>
2017-10-03 11:06:12 -0500

    Adds Gulp method to uglify and minify scripts.
    Default method also watches scripts for changes.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,16 @@ const webserver = require('gulp-webserver');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const htmlmin = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+
+/***************************************************/
+/*Paths*/
+/**************************************************/
+const   jsFiles = 'src/js/*.js';
+const   jsDest =  'dist/js'
+
 
 
 /***************************************************/
@@ -33,6 +43,15 @@ gulp.task('sass', function(){
   .pipe(gulp.dest('dist/css'))
 });
 
+gulp.task('scripts',function(){
+  return gulp.src(jsFiles)
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest(jsDest))
+  .pipe(rename('scripts.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(jsDest));
+});
+
 
 /**************************************************/
 /*watch squad*/
@@ -42,8 +61,12 @@ gulp.task('watch-sass',function(){
   gulp.watch('src/sass/*.scss',['sass'])
 });
 
+gulp.task('watch-scripts', function(){
+  gulp.watch(jsFiles, ['scripts'])
+})
+
 /**************************************************/
 /*default*/
 /**************************************************/
 
-gulp.task('default',['sass','watch-sass']);
+gulp.task('default',['sass','watch-sass', 'scripts', 'watch-scripts']);
